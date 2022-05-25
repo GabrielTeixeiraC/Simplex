1aeb5135d34e90b82153c147cf2ccdd8784c0a19
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-05-25 16:40:39 -0300

    Primal simplex

--- simplex.py
+++ simplex.py
@@ -15,16 +15,31 @@ def canonicalForm(A, c, base):
     c = np.around(c, decimals=4)
     return A, c
 
-# def removeLinearlyDependent(A):
-#     for i in range(N):
-#         for
-#     return A, c
+def removeLinearlyDependent(A, b, N):
+    for i in range(N):
+        if (A[i][i] != 0):
+            A[i, :] /= A[i][i]
+        for j in range(N):
+            if (i != j):
+                aPivot = A[j][i] * -1
+                A[j, :] += A[i, :] * aPivot
+    if(np.all(A[-1] == 0)):
+        A = np.delete(A, -1, 0)
+        b = np.delete(b, -1, 0)
+        N -= 1
+    A = np.around(A, decimals=4)
+    return A, b, N
 
 def findPivotToEnterBase(A, c):
+    N = A.shape[0]
     for j in range(N, len(c)):
+        unbounded = False
         columnIndex = j
         lineIndex = 0
         if (c[columnIndex] < 0):
+            if (np.all(A[:, columnIndex] <= 0)):
+                unbounded = True
+                break
             min =  1000
             lineIndex = 0
             for i in range(N):
@@ -35,7 +50,8 @@ def findPivotToEnterBase(A, c):
                     min = value
                     lineIndex = i
             break
-    return lineIndex, columnIndex
+
+    return lineIndex, columnIndex, unbounded
 
 def printC(c):
     for i in range(len(c)):
@@ -50,6 +66,42 @@ def printA(A):
         print()
     print()
 
+def findSolution(A, c, base):
+    N = A.shape[0]
+    solution = np.zeros(len(c) - N)
+    for i in range(len(base)):
+        solution[base[i] - N] = A[i][-1]
+    return solution
+
+def simplex(A, c, base):
+    A, c = canonicalForm(A, c, base)
+    while(np.any(c[N:len(c) - 1] < 0)):
+        lineIndex, columnIndex, unbounded = findPivotToEnterBase(A, c)
+       
+        if (unbounded):
+            d = np.zeros(len(c))
+            d[columnIndex] = 1
+            for i in range(len(base)):
+                d[i] = -1 * A[base[i]][i]
+            certificate = d
+            optimal = 'unbounded'
+            solution = findSolution(A, c, base)
+            return certificate, optimal, solution
+
+        base[lineIndex] = columnIndex
+        A, c = canonicalForm(A, c, base)
+
+    solution = findSolution(A, c, base)
+    
+    certificate = c[:N]
+    optimal = c[-1] 
+    return certificate, optimal, solution
+
+def printArray(array):
+    for i in range(len(array)):
+        print(array[i], end=' ')
+    print()
+
 N, M = input().split()    # number of restrictions and variables
 N = int(N)
 M = int(M)
@@ -62,45 +114,51 @@ for i in range(N):
 
 restrictions = np.array(restrictions, dtype=float)
 
-c = np.array(cStr, dtype=float)
+c = np.array(cStr, dtype=float) * -1
 b = np.array(restrictions[:, -1])
 A = np.array(restrictions[:,:-1])
 
 b = b.reshape(N, 1)
 
-auxiliaryA = np.eye(N)
-# A = np.concatenate((A, b), axis=1)
-sef = np.eye(N)
+A = np.concatenate((A, np.eye(N)), axis=1)
+
 for i in range(N):
     if (b[i] < 0):
         b[i] *= -1
         A[i] *= -1
-        sef[i] *= -1
 
-auxiliaryA = np.concatenate((auxiliaryA, A), axis=1)
-auxiliaryA = np.concatenate((auxiliaryA, sef), axis=1)
+auxiliaryA = np.concatenate((np.eye(N), A), axis=1)
 auxiliaryA = np.concatenate((auxiliaryA, np.eye(N)), axis=1)
 auxiliaryA = np.concatenate((auxiliaryA, b), axis=1)
 
-numColumns = auxiliaryA.shape[1]
-
-auxiliaryC = np.zeros(numColumns)
-auxiliaryC[numColumns - (N + 1):-1] = 1
+A = np.concatenate((np.eye(N), A), axis=1)
+A = np.concatenate((A, b), axis=1)
 
 
-base = list(range(numColumns - (N + 1), len(auxiliaryC) - 1))
-A = auxiliaryA
+numColumnsAuxiliary = auxiliaryA.shape[1]
 
+auxiliaryC = np.zeros(numColumnsAuxiliary)
+auxiliaryC[numColumnsAuxiliary - (N + 1):-1] = 1
 
-printC(np.around(auxiliaryC, decimals=8))
-printA(np.around(auxiliaryA, decimals=8))
-canonicalForm(auxiliaryA, auxiliaryC, base)
-while(np.any(auxiliaryC[N:len(auxiliaryC) - 1] < 0)):
-    printC(np.around(auxiliaryC, decimals=8))
-    printA(np.around(auxiliaryA, decimals=8))
-    lineIndex, columnIndex = findPivotToEnterBase(auxiliaryA, auxiliaryC)
-    base[lineIndex] = columnIndex
-    auxiliaryA, auxiliaryC = canonicalForm(auxiliaryA, auxiliaryC, base)
-printC(np.around(auxiliaryC, decimals=8))
-printA(np.around(auxiliaryA, decimals=8))
-print(base)
+auxiliaryBase = list(range(numColumnsAuxiliary - (N + 1), numColumnsAuxiliary - 1))
+
+certificate, optimal, solution = simplex(auxiliaryA, auxiliaryC, auxiliaryBase)
+if(optimal < 0):
+    print("inviavel")
+    printArray(certificate)
+else:
+    numColumns = A.shape[1]
+    c = np.concatenate((np.zeros(N), c))
+    c = np.concatenate((c, np.zeros(N)))
+    c = np.concatenate((c, np.array([0])))
+    base = list(range(numColumns - (N + 1), numColumns - 1))
+    certificate, optimal, solution = simplex(A, c, base)
+    if(optimal == 'unbounded'):
+        print("ilimitada")
+
+    else:
+        print("otima")
+        print(optimal)
+        printArray(solution[:M])
+        printArray(certificate)
+        print("FIM")
