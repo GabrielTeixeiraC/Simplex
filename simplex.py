9b0847a3aa2053fe6efc8cb4694b402de4c30602
Gabriel Teixeira <Gteixeiraca@gmail.com>
2022-05-25 12:27:59 -0300

    Auxiliary simplex working

--- simplex.py
+++ simplex.py
@@ -1,57 +1,24 @@
 import numpy as np
 
-N, M = input().split()    # number of restrictions and variables
-N = int(N)
-M = int(M)
-
-numColumns = M + N
-c = np.zeros(numColumns)    # objective function + operations log
-b = np.zeros(N)        # restrictions
-I = np.eye(N)   
-zerosA = np.zeros((N, M))
-A = np.concatenate((I, zerosA), axis=1)    # matrix of restrictions + operations log
-
-cStr = input().split()
-restrictions = []
-
-for i in range(N):
-    restrictions.append(input().split())
-
-for j in range(M):
-    c[M + j] = float(cStr[j])
-    
-for i in range(N):
-    for j in range(M):
-        A[i][M + j] = float(restrictions[i][j])
-    b[i] = float(restrictions[i][M])
-b = b.reshape(N, 1)
-auxiliaryA = A
-A = np.concatenate((A, b), axis=1)
-for i in range(N):
-    if (A[i][-1] < 0):
-        auxiliaryA[i] *= -1
-auxiliaryA = np.concatenate((auxiliaryA, np.eye(N)), axis=1)
-auxiliaryA = np.concatenate((auxiliaryA, np.absolute(b)), axis=1)
-
-onesC = np.ones(N)
-auxiliaryC = np.concatenate((np.zeros(numColumns), onesC))
-auxiliaryC = np.concatenate((auxiliaryC, np.array([0])))
-
-
-base = list(range(N + M, len(auxiliaryC) - 1))
-A = auxiliaryA
-
 # canonical form
 def canonicalForm(A, c, base):
     for i in range(len(base)):
         if (A[i][base[i]] != 0):
             A[i, :] /= A[i][base[i]]
-        for j in range(N):
+        for j in range(len(base)):
             if (i != j):
                 aPivot = A[j][base[i]] * -1
                 A[j, :] += A[i, :] * aPivot
         cPivot = c[base[i]] * -1
         c += A[i, :] * cPivot
+    A = np.around(A, decimals=4)
+    c = np.around(c, decimals=4)
+    return A, c
+
+# def removeLinearlyDependent(A):
+#     for i in range(N):
+#         for
+#     return A, c
 
 def findPivotToEnterBase(A, c):
     for j in range(N, len(c)):
@@ -64,7 +31,7 @@ def findPivotToEnterBase(A, c):
                 value = 1000
                 if (A[i][columnIndex] != 0):
                     value = A[i][-1]/A[i][columnIndex]
-                if(value < min and value > 0):
+                if(value < min and value >= 0 and A[i][columnIndex] > 0):
                     min = value
                     lineIndex = i
             break
@@ -72,27 +39,68 @@ def findPivotToEnterBase(A, c):
 
 def printC(c):
     for i in range(len(c)):
-        print(round(c[i],2), end='\t')
+        print(c[i], end='\t')
     print()
     print()
 
 def printA(A):
     for i in range(len(A)):
         for j in range(len(A[0])):
-            print(round(A[i][j], 2), end='\t')
+            print(A[i][j], end='\t')
         print()
     print()
 
-printC(auxiliaryC)
-printA(auxiliaryA)
-canonicalForm(A, auxiliaryC, base)
-printC(auxiliaryC)
-printA(auxiliaryA)
-while(np.any(auxiliaryC[N:] < 0)):
-    printC(auxiliaryC)
-    printA(auxiliaryA)
-    lineIndex, columnIndex = findPivotToEnterBase(A, auxiliaryC)
+N, M = input().split()    # number of restrictions and variables
+N = int(N)
+M = int(M)
+
+cStr = input().split()
+restrictions = []
+
+for i in range(N):
+    restrictions.append(input().split())
+
+restrictions = np.array(restrictions, dtype=float)
+
+c = np.array(cStr, dtype=float)
+b = np.array(restrictions[:, -1])
+A = np.array(restrictions[:,:-1])
+
+b = b.reshape(N, 1)
+
+auxiliaryA = np.eye(N)
+# A = np.concatenate((A, b), axis=1)
+sef = np.eye(N)
+for i in range(N):
+    if (b[i] < 0):
+        b[i] *= -1
+        A[i] *= -1
+        sef[i] *= -1
+
+auxiliaryA = np.concatenate((auxiliaryA, A), axis=1)
+auxiliaryA = np.concatenate((auxiliaryA, sef), axis=1)
+auxiliaryA = np.concatenate((auxiliaryA, np.eye(N)), axis=1)
+auxiliaryA = np.concatenate((auxiliaryA, b), axis=1)
+
+numColumns = auxiliaryA.shape[1]
+
+auxiliaryC = np.zeros(numColumns)
+auxiliaryC[numColumns - (N + 1):-1] = 1
+
+
+base = list(range(numColumns - (N + 1), len(auxiliaryC) - 1))
+A = auxiliaryA
+
+
+printC(np.around(auxiliaryC, decimals=8))
+printA(np.around(auxiliaryA, decimals=8))
+canonicalForm(auxiliaryA, auxiliaryC, base)
+while(np.any(auxiliaryC[N:len(auxiliaryC) - 1] < 0)):
+    printC(np.around(auxiliaryC, decimals=8))
+    printA(np.around(auxiliaryA, decimals=8))
+    lineIndex, columnIndex = findPivotToEnterBase(auxiliaryA, auxiliaryC)
     base[lineIndex] = columnIndex
-    canonicalForm(A, auxiliaryC, base)
-printC(auxiliaryC)
-printA(auxiliaryA)
+    auxiliaryA, auxiliaryC = canonicalForm(auxiliaryA, auxiliaryC, base)
+printC(np.around(auxiliaryC, decimals=8))
+printA(np.around(auxiliaryA, decimals=8))
+print(base)
